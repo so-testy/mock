8c097155aaf7a02f4745842b749a87ba81908e67
Nepein Andrey <nepein96@gmail.com>
2020-05-04 18:13:51 +0300

    exams

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -1,54 +1,131 @@
 import socketIO from 'socket.io';
 
-const users = [{
-    id: 0,
-    name: null,
-    socket: null,
-    token: '3443fsdflksdjfsfs',
-    role: 'admin'
-}, {
-    id: 1,
-    name: null,
-    socket: null,
-    token: 'sflk4j3lk4j34lk34',
-    role: 'user'
-}, {
-    id: 2,
-    name: null,
-    socket: null,
-    token: '5kn6589h5nh856n5g',
-    role: 'user'
-}, {
-    id: 3,
-    name: null,
-    socket: null,
-    token: 'jg0945jg405gjjlkg',
-    role: 'user'
-}]
+const users = [];
+// const activeUsers = [];
+const messages = [];
+const roomOpts = { maxWorking: 2, timer: 20 };
+const exam = {
+  started: false,
+  startTime: null,
+  studentId: null,
+  queue: {
+    waiting: [],
+    working: [],
+    ready: [],
+  },
+};
 
 export let io;
-export const initSocket = server => {
-    io = socketIO(server);
+export const initSocket = (server) => {
+  io = socketIO(server);
 
-    io.on('connection', socket => {
-        console.log('a user connected');
+  io.on('connection', (socket) => {
+    console.log(socket.id, 'a user connected');
 
-        // authorization
-        socket.on('get-profile', token => {
-            socket.emit('get-profile', users.find(u => u.token == token));
-        });
+    // регастрация юзера
+    socket.on('createUser', (data) => {
+      let user = users.find((u) => u.id == socket.id);
 
-        // chatting
-        socket.on('message', ({ msg, token }) => {
-            const user = users.find(u => u.token == token);
-            io.sockets.emit('message', {
-                msg,
-                user,
-            });
-        });
+      if (!user) {
+        user = { name: socket.id, id: socket.id, type: data.type, status: 'waiting', statusChanged: new Date() };
+        users.push(user);
+        if (data.type !== 'teacher') exam.queue.waiting.push(user);
+        // activeUsers.push(user);
+      }
 
-        socket.on('disconnect', () => {
-            console.log('A user disconnected');
-        });
+      socket.emit('userData', user);
+      io.sockets.emit('users', { users: exam.queue, participants: users });
+      io.sockets.emit('examData', exam);
+
+      socket.emit('messages', messages);
     });
-}
+
+    // начинается экзамен
+    socket.on('startExam', () => {
+      if (!exam.started) {
+        exam.started = true;
+        exam.startTime = new Date();
+      }
+
+      const queueVolume = roomOpts.maxWorking - exam.queue.working.length;
+      for (let i = 0; i < queueVolume; i++) {
+        const el = exam.queue.waiting.shift();
+        el && exam.queue.working.push(el);
+      }
+
+      io.sockets.emit('examData', exam);
+    });
+
+    // начинается защита
+    socket.on('startStudentExam', (data) => {
+      exam.studentId = data.id;
+      io.sockets.emit('examData', exam);
+    });
+
+    // экзамен сдан
+    socket.on('examPassed', (data) => {
+      const id = exam.queue.ready.findIndex(u => u.id === data.id);
+      exam.queue.ready.splice(id, 1);
+      exam.studentId = null;
+      if (exam.queue.ready.length + exam.queue.waiting.length + exam.queue.working.length === 0) exam.started = false;
+
+      io.sockets.emit('examData', exam);
+    });
+
+    // юзер готов к сдаче
+    socket.on('userReady', () => {
+
+      const userIndex = exam.queue.working.findIndex((u) => u.id === socket.id);
+
+      if (userIndex !== -1) {
+        const user = exam.queue.working[userIndex];
+
+        exam.queue.working.splice(userIndex, 1);
+        exam.queue.ready.push(user);
+        const queueVolume = roomOpts.maxWorking - exam.queue.working.length;
+
+        for (let i = 0; i < queueVolume; i++) {
+          const el = exam.queue.waiting.shift();
+          el && exam.queue.working.push(el);
+        }
+
+        io.sockets.emit('examData', exam);
+      }
+    });
+
+    socket.on('message', (message) => {
+      messages.push({ ...message, myself: false });
+      io.sockets.emit('messages', messages);
+    });
+
+    socket.on('disconnect', () => {
+      Promise.race([
+        new Promise((res, rej) => {
+          const userIndex = exam.queue.waiting.findIndex((u) => u.id == socket.id);
+          if (userIndex !== -1) {
+            exam.queue.waiting.splice(userIndex, 1);
+            res();
+          }
+        }),
+        new Promise((res, rej) => {
+          const userIndex = exam.queue.working.findIndex((u) => u.id == socket.id);
+          if (userIndex !== -1) {
+            exam.queue.working.splice(userIndex, 1);
+            res();
+          }
+        }),
+        new Promise((res, rej) => {
+          const userIndex = exam.queue.ready.findIndex((u) => u.id == socket.id);
+          if (userIndex !== -1) {
+            exam.queue.ready.splice(userIndex, 1);
+            res();
+          }
+        }),
+      ]);
+
+      io.sockets.emit('users', { users: exam.queue, participants: users });
+
+      console.log('A user disconnected');
+    });
+  });
+};
